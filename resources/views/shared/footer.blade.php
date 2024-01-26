5a44bd1a7d45d52a1b00bddb5f0cc0c871ce43f8
rebs-code <jack.rebussi@gmail.com>
2024-01-26 20:27:14 +0100

    feat: bottom footer

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,1 +1,42 @@
-<footer>socials</footer>
+<footer class="bottom-footer">
+    <div class="bottom-footer-container container">
+        <div class="bottom-footer-left">
+            <button class="footer-button">Sign-up Now!</button>
+        </div>
+        <div class="bottom-footer-menu">
+            <div class="bottom-footer-menu-item">
+                <h3 class="text-uppercase">Follow us</h3>
+                <ul>
+                    <li>
+                        <a :href="">
+                            <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a :href="">
+                            <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a :href="">
+                            <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a :href="">
+                            <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a :href="">
+                            <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+
+        </div>
+    </div>
+    </div>
+</footer>
