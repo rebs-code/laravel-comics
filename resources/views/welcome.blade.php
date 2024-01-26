2c21d37da1ba47e32a065cf76f1907266df56f23
rebs-code <jack.rebussi@gmail.com>
2024-01-26 19:32:15 +0100

    fix: main section style in welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.main')
 
 @section('main')
-    <main class="main-container">
+    <div class="main-container">
         <div class="container relative">
             <div class="title-label">
                 <h3>Current Series</h3>
             </div>
         </div>
         <div class="bottom-container">
-            <div class="container-large comics-container">
+            <div class="container comics-container">
                 @foreach ($comics as $comic)
-                    <div>
+                    <div class="comic">
                         <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
-                        <h3>{{ $comic['series'] }}</h3>
+                        <h3 class="text-uppercase text-center">{{ $comic['series'] }}</h3>
                     </div>
                 @endforeach
                 <button>Load More</button>
             </div>
-    </main>
-@endsection
+        </div>
+    @endsection
