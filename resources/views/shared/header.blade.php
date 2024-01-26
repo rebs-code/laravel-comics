84fc4abfd7f07ebd3aab9c16dad43e489436c4d8
rebs-code <jack.rebussi@gmail.com>
2024-01-26 18:53:01 +0100

    Add header styles and reset styles

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -5,16 +5,16 @@
         </div>
         <div class="header-menu">
             <ul class="text-uppercase">
-                <li>Characters</li>
-                <li>Comics</li>
-                <li>Movies</li>
-                <li>TV</li>
-                <li>Games</li>
-                <li>Collectibles</li>
-                <li>Videos</li>
-                <li>Fans</li>
-                <li>News</li>
-                <li>Shop</li>
+                <li><a href="/characters">Characters</a></li>
+                <li><a href="/comics">Comics</a></li>
+                <li><a href="/movies">Movies</a></li>
+                <li><a href="/tv">TV</a></li>
+                <li><a href="/games">Games</a></li>
+                <li><a href="/collectibles">Collectibles</a></li>
+                <li><a href="/videos">Videos</a></li>
+                <li><a href="/fans">Fans</a></li>
+                <li><a href="/news">News</a></li>
+                <li><a href="/shop">Shop</a></li>
             </ul>
         </div>
     </div>
