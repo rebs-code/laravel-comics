9b22261729ebaab3c372713fb879e9139b9ec116
rebs-code <jack.rebussi@gmail.com>
2024-01-26 17:56:52 +0100

    create: shared header and footer

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,7 +8,7 @@
     @vite('resources/js/app.js')
 </head>
 <body>
-    <header>logo = menu</header>
+    <header>logo - menu</header>
     <main>comics</main>
     <footer>socials</footer>
     {{-- <img src="{{ Vite::asset('resources/images/logo.png') }}"> --}}
