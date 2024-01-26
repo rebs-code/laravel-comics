47307cc61d80b479117fea43f0f41e3ddcbde853
rebs-code <jack.rebussi@gmail.com>
2024-01-26 19:21:36 +0100

    Add main.scss and variables.scss, update app.scss and welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,27 +1,21 @@
 @extends('layouts.main')
 
 @section('main')
-<main class="main-container">
-    <div class="container-large relative">
-        <div class="title-label">
-            <h3>Current Series</h3>
-        </div>
-    </div>
-    <div class="bottom-container">
-        <div class="container-large comics-container">
-            @foreach ($comics as $comic)
-            <div class="comic">
-                <a href="">
-                    <div class="comic-img">
-                        <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
-                    </div>
-                    <div class="comic-title">
-                        <h4>{{ $comic['title'] }}</h4>
-                    </div>
-                </a>
+    <main class="main-container">
+        <div class="container relative">
+            <div class="title-label">
+                <h3>Current Series</h3>
             </div>
-            @endforeach
-        <button>Load More</button>
-    </div>
-</main>
+        </div>
+        <div class="bottom-container">
+            <div class="container-large comics-container">
+                @foreach ($comics as $comic)
+                    <div>
+                        <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                        <h3>{{ $comic['series'] }}</h3>
+                    </div>
+                @endforeach
+                <button>Load More</button>
+            </div>
+    </main>
 @endsection
