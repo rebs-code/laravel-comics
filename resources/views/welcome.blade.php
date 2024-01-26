ad7c39a2cb1b8ec51584b213721947eca41f12d5
rebs-code <jack.rebussi@gmail.com>
2024-01-26 19:02:43 +0100

    feat: main with comics view from db

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,27 @@
 @extends('layouts.main')
 
 @section('main')
-    @foreach ($comics as $comic)
-       
-        
-    @endforeach
+<main class="main-container">
+    <div class="container-large relative">
+        <div class="title-label">
+            <h3>Current Series</h3>
+        </div>
+    </div>
+    <div class="bottom-container">
+        <div class="container-large comics-container">
+            @foreach ($comics as $comic)
+            <div class="comic">
+                <a href="">
+                    <div class="comic-img">
+                        <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                    </div>
+                    <div class="comic-title">
+                        <h4>{{ $comic['title'] }}</h4>
+                    </div>
+                </a>
+            </div>
+            @endforeach
+        <button>Load More</button>
+    </div>
+</main>
 @endsection
